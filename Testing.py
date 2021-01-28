c3aa49be54321171a9549fd795d63b952c73f4a2
PinetheApple <75367614+PinetheApple@users.noreply.github.com>
2021-01-28 18:40:17 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,3 +1,13 @@
 import discord
 from discord.ext import commands
 
+import asyncio
+bot = commands.Bot(command_prefix='p!')
+@bot.event
+async def on_ready():
+    print("The bot is ready!")
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+bot.run('P_Bot')
+
