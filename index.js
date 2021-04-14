8bf68cf58ff7a81ef2f3c3eedfc1970a5783c3a9
Pine <75367614+PinetheApple@users.noreply.github.com>
2021-04-14 18:39:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', message => {
-	if (message.content === 't!ping') {
-        // send back "Pong." to the channel the message was sent in
+	if (message.content === `${prefix}ping`) {
         message.channel.send('Pong.');
+    } else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
     }
 });
-client.login(config.token);
+client.login(token);
