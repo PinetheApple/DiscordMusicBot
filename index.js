937c756e49123f75a672ad9b3b4f3662e9c42195
Pine <75367614+PinetheApple@users.noreply.github.com>
2021-04-14 19:24:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', message => {
-	if (message.content === `${prefix}ping`) {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+	if (command === `ping`) {
         message.channel.send('Pong.');
-    } else if (message.content === `${prefix}beep`) {
+    } else if (command === `beep`) {
         message.channel.send('Boop.');
     }
 });
