897c1da53b5aa731756cfe65d6752a6ab8f45ad4
Pine <75367614+PinetheApple@users.noreply.github.com>
2022-04-17 18:05:12 +0530

    Update music.py
    fixed the error thrown when the song is completed

--- music.py
+++ music.py
@@ -1,7 +1,16 @@
+from http import client
 import discord
 from discord.ext import commands
 import youtube_dl
 from requests import get
+import asyncio
+
+class Queue():
+    def _init_(self,song):
+        self.song=song
+        self.next=None
+
+    
 
 class music(commands.Cog):
     def _init_(self,client):
@@ -17,6 +26,9 @@ class music(commands.Cog):
                 video = ydl.extract_info(arg, download=False)
 
         return video
+
+    def play(vc):
+        e='e'
 
     @commands.command(aliases=['j'])
     async def join(self,ctx):
@@ -34,8 +46,17 @@ class music(commands.Cog):
         await ctx.voice_client.disconnect()
 
     @commands.command()
-    async def play(self,ctx,url):
-        ctx.voice_client.stop()
+    async def volume(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def play(self,ctx):
+        #ctx.voice_client.stop()
+        url=ctx.message.content.split("t!play ",1)[1]
         FFMPEG_OPTIONS={"options":"-vn"}
         YDL_OPTIONS={'format':"bestaudio"}
         vc = ctx.voice_client
@@ -44,8 +65,11 @@ class music(commands.Cog):
             info=music.search(url,YDL_OPTIONS)
             url2= info['formats'][0]['url']
             source= await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+            await ctx.send(f"Now Playing: **{url}**")
             vc.play(source)
-            await ctx
+            while ctx.voice_client.is_playing():
+                await asyncio.sleep(1)
+            print("song ended boop")
 
     @commands.command(aliases=['stop'])
     async def pause(self,ctx):
@@ -56,6 +80,17 @@ class music(commands.Cog):
     async def resume(self,ctx):
         await ctx.voice_client.resume()
         await ctx.send("Resumed.")
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
 
 def setup(client):
     client.add_cog(music(client))
