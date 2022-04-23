71a99d3037f45a146efb1ba5f2a10f80c4cf23e3
Pine <75367614+PinetheApple@users.noreply.github.com>
2022-04-23 18:14:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,28 @@ import disnake
 from disnake.ext import commands
 import music
 
-client=commands.Bot(command_prefix='t!',intents=discord.Intents.all())
+act = disnake.Game(name='Hopefully a music bot')
+bot=commands.Bot(command_prefix="t!",activity=act)
+bot.color=0xff0000
 
 cogs=[music]
 
 for i in range(len(cogs)):
-    cogs[i].setup(client)
+    cogs[i].setup(bot)
 
-@client.event
+@bot.event
 async def on_ready():
-  print("Logged in as {0.user}".format(client))
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def test(ctx):
+    await ctx.channel.send("HEllO")
+
+@bot.command()
+async def ping(ctx):
+    """Check my reaction time!"""
+    resp = await ctx.send('Loading...')
+    diff = resp.created_at - ctx.message.created_at
+    await resp.edit(content=f':ping_pong: Pong! **API** latency: {1000*diff.total_seconds():.1f}ms. **{bot.user.name}** latency: {round(bot.latency * 1000)}ms')
 
 client.run("token")
